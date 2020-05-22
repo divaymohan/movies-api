c294c689f6dd632d4d25ef0db90abffe5699789d
divaymohan <divaymohan05@gmail.com>
2020-05-22 10:42:08 +0530

    CRUD working and tested on genre end point

--- routes/genre.js
+++ routes/genre.js
@@ -21,8 +21,16 @@ route.get('/',async (req,res)=>{
     }
 });
 route.get('/:id',async (req,res)=>{
+    try{
+        const genre = await genreDB.getGenreById(req.params.id);
+        if(!genre) res.status(400).send(`No genre found with id ${req.params.id}`);
+        res.send(genre);
+    }catch(err){
+        res.send(err.message);
+    }
 
 });
+
 route.post('/',async (req,res)=>{
     const {err,value} = validateRequest(req.body);
     if(err) res.status(400).send(err.message);
@@ -30,6 +38,29 @@ route.post('/',async (req,res)=>{
         const result = await genreDB.addNewGenre(req.body);
         res.send(result);
 
+    }
+    catch(err){
+        res.send(err.message);
+    }
+});
+route.put('/:id',async (req,res)=>{
+    let {err,val} = validateRequest(req.body);
+    if(err) res.status(400).send(err.message);
+    try{
+        const result = await genreDB.updateGenre(req.params.id,req.body);
+        if(!result) res.status(400).send(`No genre found with id ${req.params.id}`);
+        res.send(result);
+     }
+     catch(err){
+         res.send(err.message);
+     }
+    
+});
+route.delete('/:id',async (req,res)=>{
+    try{
+        const result = await genreDB.deleteGenre(req.params.id);
+        if(!result) res.status(400).send(`No genre found with id ${req.params.id}`);
+        res.send(result);
     }
     catch(err){
         res.send(err.message);
