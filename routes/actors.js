3220746312a7f620355e0e360612d5bed371716c
divaymohan <divaymohan05@gmail.com>
2020-05-22 11:01:46 +0530

    starting actor ecnd point

---
+++ routes/actors.js
@@ -0,0 +1,4 @@
+const expresss = require('express');
+const route = expresss.Router();
+
+module.exports = route;
--- routes/movies.js
+++ routes/movies.js
@@ -2,7 +2,9 @@ const express =  require('express');
 const route = express.Router();
 
 const genreRoute = require('./genre');
+const actorRoute = require('./actors');
 
 route.use('/genre',genreRoute); 
+route.use('/actor',actorRoute);
 
 module.exports = route;
